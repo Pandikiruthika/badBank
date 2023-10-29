57c42bd3888aa12ca0862285ac6a4a43e7af9932
Pandikiruthika <111949410+Pandikiruthika@users.noreply.github.com>
2023-10-28 22:26:33 -0700

    Delete badbank-backend directory

--- badbank-backend/config/db.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose= require("mongoose");
-mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true})
-.then(
-    ()=>console.log("mongoDb is connected")
-)
-.catch(
-    (err)=>console.log(err)
-)
